7f71ac942f3dd7712148f228a6f943553dc5eccb
Radhey Khandelwal <radheykhandelwal2000@gmail.com>
2022-07-05 12:26:47 +0530

    push

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+
+const dbURI =`mongodb+srv://raadheyyy:Ajay123@@lms.isz2snt.mongodb.net/?retryWrites=true&w=majority` //add link here
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(process.env.PORT || 3000))
+  .catch((err) => console.log(err));
+
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.get('/data', requireAuth, (req, res) => res.render('data'));
+app.get('/fullstack', requireAuth, (req, res) => res.render('fullstack'));
+app.use(authRoutes);
